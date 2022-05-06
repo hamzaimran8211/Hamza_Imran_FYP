19e6a5879aa4fa773bb185fdbc7639668b59a7ca
u2018267 <u2018267@giki.edu.pk>
2022-05-06 16:14:18 +0500

    first commit

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -5,25 +5,23 @@ import Footer from './Footer';
 
 
 
-const Layout = ({children}) =>(
-    <>
-    <Head>
-        <title>
-            Real Estate
-        </title>
-    </Head>
-    <Box maxWidth="1280px" m="auto">
-        <header>
-            <Navbar />
-        </header>
-        <main>
-            {children}
-        </main>
-        <footer>
-            <Footer />
-        </footer>
-    </Box>
-    </>
-);
 
-export default Layout;
+export default function Layout({ children }) {
+    return (
+      <>
+        <Head>
+          <title>Real Estate</title>
+        </Head>
+        <Box maxWidth='1280px' m='auto'>
+          <header>
+            <Navbar />
+          </header>
+          <main>{children}</main>
+          <footer>
+            <Footer />
+          </footer>
+        </Box>
+      </>
+    );
+  }
+  
